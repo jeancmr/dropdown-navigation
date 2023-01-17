e349474ba40a9ac2a1347f7f4f0ae87f459c8a3f
jeancmr <jean.madiedo08@gmail.com>
2023-01-16 23:01:34 -0500

    burger menu added

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-header");
+  const menuOverlay = document.querySelector(".menu-overlay");
+
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
+    //burger animation
+    burger.classList.toggle("toggle");
+    //toggle landscape
+    menuOverlay.classList.toggle("active");
+  });
+  // closeNavMenu.addEventListener("click", toggleNav);
+  // close the navMenu by clicking outside
+  // menuOverlay.addEventListener("click", function toggleNav() {
+  //   // navMenu.classList.toggle("open");
+  //   menuOverlay.classList.toggle("active");
+  //   // document.body.classList.toggle("hidden-scrolling");
+  // });
+};
+navSlide();
+
 // const btnNavDrop = document.querySelector(".btn-dropdown");
 // const ulMenu = document.querySelector(".dp-menu");
 // // const arrow = document.querySelector(".arrow-down");
@@ -7,19 +30,20 @@
 //   // arrow.classList.toggle("arrow-up");
 // });
 
-const btnNavDrop = document.querySelectorAll(".btn-dropdown");
-const ulMenu = document.querySelectorAll(".dp-menu");
-// const arrow = document.querySelector(".arrow-down");
-btnNavDrop.forEach(function (btnDrop) {
-  btnDrop.addEventListener("click", function () {
-    ulMenu.forEach(function (ulMenus) {
-      ulMenus.classList.toggle("dp-menu-open");
-      console.log(ulMenus);
-    });
-    // ulMenuLi.classList.toggle("dp-menu-open");
-    console.log(ulMenu);
-  });
-});
+// RECENT ONE
+// const btnNavDrop = document.querySelectorAll(".btn-dropdown");
+// const ulMenu = document.querySelectorAll(".dp-menu");
+// // const arrow = document.querySelector(".arrow-down");
+// btnNavDrop.forEach(function (btnDrop) {
+//   btnDrop.addEventListener("click", function () {
+//     ulMenu.forEach(function (ulMenus) {
+//       ulMenus.classList.toggle("dp-menu-open");
+//       console.log(ulMenus);
+//     });
+//     // ulMenuLi.classList.toggle("dp-menu-open");
+//     console.log(ulMenu);
+//   });
+// });
 // const btnNavDrop = document.querySelectorAll(".btn-dropdown");
 // btnNavDrop.forEach((papa) => {
 //   papa.addEventListener("click", function () {
